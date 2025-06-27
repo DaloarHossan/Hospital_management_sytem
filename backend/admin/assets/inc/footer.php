8a3253cfe369310e1ca25a51575e9d3932ea2f8a
unknown <daloarhossan128@gmail.com>
2025-06-28 01:31:18 +0800

    upload files

---
+++ backend/admin/assets/inc/footer.php
@@ -0,0 +1,10 @@
+<footer class="footer">
+    <div class="container-fluid">
+        <div class="row text-center">
+            <div class="col-md-12">
+                <?php echo date('Y'); ?> &copy; Hospital Management System</a>
+            </div>
+
+        </div>
+    </div>
+</footer>
